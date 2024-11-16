da3e3a68f887367f4fecd2803835851ebe69f45e
Mahir <mkojak75@gmail.com>
2024-11-16 11:10:07 +0100

    up

---
+++ NodeJs_Modules/index.js
@@ -0,0 +1,44 @@
+"use strict";
+//? Object
+// const obj= require("./modules")
+
+// console.log(obj);
+
+//? Array
+
+// const arr = require("./modules")
+
+// console.log(arr);
+// console.log(arr,[]);
+
+//? Function
+//*Array
+// const arr = require("./modules")
+// arr[0]()
+
+// const [a,b,c] = require("./modules")
+// a()
+// b()
+// c()
+
+//* Object
+// const obj = require("./modules")
+// obj.func1()
+
+// const {func1, func2, func3}= require("./modules")
+// func1()
+
+
+const { func1, func2, func3 } = require("./modules");
+
+
+
+
+
+//*Built-in packages
+// require("node:http")
+// require("http")
+// require("node:")
+
+// require("dotenv").config();
+// console.log(process.env);
--- NodeJs_Modules/modules/index.js
+++ NodeJs_Modules/modules/index.js
@@ -1,2 +1,80 @@
 "use strict"
 
+/*----------------------------------*
+console.log("./modules indexten gelen");
+
+const x = 10
+const y = 5
+
+// module.exports = {x, y}
+module.exports = [x, y]
+
+
+/*----------------------------------*
+//* Function Declaration
+function func0 (){
+console.log("func0 calisti");
+}
+
+const func1 = function () {
+    console.log("func1 calisti");
+}
+const func2 = function () {
+    console.log("func2 calisti");
+}
+const func3 = function () {
+    console.log("func3 calisti");
+}
+
+module.exports = [func1, func2, func3]
+/*----------------------------------*
+//? Object
+const func1 = function () {
+    console.log("func1 calisti");
+}
+const func2 = function () {
+    console.log("func2 calisti");
+}
+const func3 = function () {
+    console.log("func3 calisti");
+}
+
+module.exports = {func1,func2,func3}
+/*----------------------------------*
+const func1 = function () {
+    console.log("func1 calisti");
+}
+const func2 = function () {
+    console.log("func2 calisti");
+}
+const func3 = function () {
+    console.log("func3 calisti");
+}
+
+module.exports.func1 = func1
+module.exports.func2 = func2
+module.exports.func3 = func3
+/*----------------------------------*/
+
+
+module.exports = {
+    func1: function () {
+        console.log("func1 calisti");
+    }
+}
+module.exports = {
+    func2: function () {
+        console.log("func3 calisti");
+    }
+}
+module.exports = {
+    func3: function () {
+        console.log("func3 calisti");
+    }
+}
+/*----------------------------------*
+
+/*----------------------------------*/
+
+/*----------------------------------*/
+/*----------------------------------*/
