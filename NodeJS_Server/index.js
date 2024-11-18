bd7ff1951ecceb6ff08d111ccf2eb971b67b1ba7
Mahir <mkojak75@gmail.com>
2024-11-18 20:28:14 +0100

    createsServer

--- NodeJS_Server/index.js
+++ NodeJS_Server/index.js
@@ -1,7 +1,7 @@
-"use strict"
+"use strict";
 //* console.log("nodejs server");
 
-const http = require("node:http")
+const http = require("node:http");
 
 // http.createServer((request,response)=>{
 
@@ -27,18 +27,57 @@ const http = require("node:http")
 //         res.end("<h1>hello FS</h1>")
 //      }).listen(8000, "127.0.0.1", ()=> console.log("server Runned http://127.0.0.1:8000"))
 
-require("dotenv").config()
-const PORT=process.env.PORT
-const HOST=process.env.HOST
+require("dotenv").config();
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+/*----------------------------------------*
+const app = http
+  .createServer((req, res) => {
+    if (req.url == "/") 
+        if(req.method=="GET")
+            res.end("<h1>Home Page</h1>");
+            
+        else
+            res.end("<h1>You can not POST</h1>")
+    else if (req.url == "/user") 
+        res.end("<h1>User page</h1>");
 
-const app = http.createServer((req,res)=>{
+    else 
+    res.end("<h1>Undefined URL</h1>");
+  })
+  .listen(PORT, () => console.log(`server Runned http://${HOST}:${PORT}`));
+
+
+/*----------------------------------------*
+  const app= http.createServer((req, res)=>{
+    if(req.url=='/' && req.method=='GET'){
+        res.write('message 1 ')
+        res.write('message 2 ')
+        res.write('message 3 ')
+    }
+    else if(req.method=='POST'){
+        // res.write('method POST')
+        // res.statusCode=201
+        // res.statusMessage='Tamam'
+        res.writeHead(201,"tamam")
+        res.write('method POST')
+    }
+        res.end()
     
-        res.end("<h1>hello FS</h1>")
-     }).listen(PORT, ()=> console.log(`server Runned http://${HOST}:${PORT}`))
+}).listen(PORT,()=> console.log(`Server Runned http://${HOST}:${PORT}`))
 
 
 /*----------------------------------------*/
-/*----------------------------------------*/
-/*----------------------------------------*/
-/*----------------------------------------*/
-/*----------------------------------------*/
+const app= http.createServer((req, res)=>{
+    
+    const obj={
+        chort:'FS',
+        message: 'Hello'
+
+    }
+    res.end(JSON.stringify(obj))
+    
+}).listen(PORT,()=> console.log(`Server Runned http://${HOST}:${PORT}`))
+
+
+
