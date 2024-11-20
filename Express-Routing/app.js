a04e172fb9ec11db4158ab3e8e9b377fb30cf4fb
Mahir <mkojak75@gmail.com>
2024-11-20 19:46:47 +0100

    get,post

--- Express-Routing/app.js
+++ Express-Routing/app.js
@@ -8,7 +8,6 @@
  * npm i express dotenv
  * echo PORT=8000 > .env
  * npm nodemon ....js
- * 
  */
 
 const express = require("express") //* express assigned
@@ -19,6 +18,52 @@ require("dotenv").config() //* .env icindeki degiskenler => process.env ye atand
 const HOST = process.env.HOST || "127.0.0.1" //* local IP
 const PORT = process.env.PORT || 8000
 
+/* ------------------------------------------------------- *
+
+app.get("/",(req, res)=>{
+
+    //? run res.send() for print-out
+    //*res.send("Welcome to express")
+    res.send({
+        message: "welcome to express"
+    })
+
+
+})
+app.post("/",(req, res)=>{
+
+    res.send({
+        message: " run post"
+    })
+
+
+})
+app.put("/",(req, res)=>{
+
+    res.send({
+        message: " run put"
+    })
+
+
+})
+app.delete("/",(req, res)=>{
+
+    res.send({
+        message: " run delete"
+    })
+
+
+})
+/* ------------------------------------------------------- */
+//* Bütün metodlara izin ver all()
+app.all("/",(req, res)=>{
+
+    res.send({
+        message: " run all"
+    })
+
+
+})
 /* ------------------------------------------------------- */
 
 //? app.listen(PORT, ()=>{console.log(`Running: http://127.0.0.1:8000`)})
