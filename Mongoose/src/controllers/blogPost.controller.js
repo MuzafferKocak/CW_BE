d3eb8ab07db112af6da8810ec3e44fb5a7aa7c31
Mahir <mkojak75@gmail.com>
2024-12-02 17:53:37 +0100

    up

--- Mongoose/src/controllers/blogPost.controller.js
+++ Mongoose/src/controllers/blogPost.controller.js
@@ -3,3 +3,23 @@
 /*------------------------------------------------
 |     //? Express - Blog Project with Mongoose
 -------------------------------------------------*/
+
+//* Call Models:
+
+//* BlogPost Controller:
+
+module.exports.blogPost = {
+    list: async (req, res) => {},
+  
+    //* CRUD ->
+  
+    create: async (req, res) => {},
+  
+    read: async (req, res) => {},
+  
+    update: async (req, res) => {},
+  
+    delete: async (req, res) => {},
+  };
+  
+  /* ------------------------------------------------------- */
