22f5d885fabedd21986e548697c3d33342ee0aa8
Mahir <mkojak75@gmail.com>
2024-12-19 19:51:19 +0100

    multer

--- PizzaApi -3/src/routes/pizza.js
+++ PizzaApi -3/src/routes/pizza.js
@@ -9,9 +9,34 @@ const router = require("express").Router();
 
 const pizza = require("../controllers/pizza");
 
+/* ------------------------------------------------------- */
+//* Uploading Multer
+const multer = require("multer");
+
+//* upload middleware:
+const upload = multer({
+  // dest: "./upload",
+  storage: multer.diskStorage({
+    destination: "./upload",
+    filename: function (req, file, returnCallback) {
+      console.log(file);
+      // returnCallback(error, fileName);
+      // returnCallback(null, file.originalname)
+      returnCallback(null, Date.now() + "_" + file.originalname);
+    },
+  }),
+});
+/* ------------------------------------------------------- */
+
 //* URL: /pizzas
 
-router.route("/").get(pizza.list).post(pizza.create);
+router
+  .route("/")
+  .get(pizza.list)
+  // .post(pizza.create);
+  // .post(upload.single("image"), pizza.create); //* tek dosya
+.post(upload.array('image'), pizza.create) //* Cok dosya (tavsiye)
+// .post(upload.any(), pizza.create) //* cok dosya fieldname önemsiz
 
 router
   .route("/:id")
