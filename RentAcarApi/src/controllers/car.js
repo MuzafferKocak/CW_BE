b52708c9c74d78715e50730d0637293e0d3fe664
Mahir <mkojak75@gmail.com>
2024-12-24 18:54:54 +0100

    models

--- RentAcarApi/src/controllers/car.js
+++ RentAcarApi/src/controllers/car.js
@@ -4,3 +4,40 @@
 -------------------------------------------------*/
 
 //* Car Controller:
+
+const Car = require("../models/car");
+
+module.exports = {
+  list: async (req, res) => {
+    let customFilter = { isAvailable: true };
+    const data = await res.getModelList(Car, customFilter, [
+      {
+        path: "createdId",
+        select: "username",
+      },
+      {
+        path: "updatedId",
+        select: "username",
+      },
+    ]);
+    res.status(200).send({
+        error: false,
+        details: await res.getModelListDetails(Car, customFilter)
+    })
+  },
+  create: async (req, res) => {
+
+    req.body.createdId = req.user._id
+    req.body.updatedId = req.user._id
+
+    const data = await Car.create(req.body)
+
+    res.status(201).send({
+        error: false,
+        data,
+    })
+  },
+  read: async (req, res) => {},
+  update: async (req, res) => {},
+  delete: async (req, res) => {},
+};
--- RentAcarApi/src/models/car.js
+++ RentAcarApi/src/models/car.js
@@ -32,3 +32,59 @@ const { mongoose } = require("../configs/dbConnection");
 }
 /* ------------------------------------------------------- */
 // Car Model:
+
+const Carschema = new mongoose.Schema(
+  {
+    plateNumber: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    brand: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    model: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    year: {
+      type: Number,
+      min: 2000,
+      max: new Date().getFullYear(),
+      required: true,
+    },
+    isAutomatic: {
+      type: Boolean,
+      default: false,
+    },
+    pricePerDay: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: Array,
+      default: [],
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    createdId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    updatedId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { collection: "cars", timestamps: true }
+);
+
+module.exports = mongoose.model("Car", Carschema);
