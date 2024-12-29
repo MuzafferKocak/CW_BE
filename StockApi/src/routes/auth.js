4731561110eb58c1bc7e32c7e2617c7099eaa0d7
Mahir <mkojak75@gmail.com>
2024-12-29 14:28:31 +0100

    authentication

--- StockApi/src/routes/auth.js
+++ StockApi/src/routes/auth.js
@@ -2,3 +2,17 @@
 /*------------------------------------------------
 |     //? Express - Stock Api
 -------------------------------------------------*/
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+//* routes/auth:
+
+const auth = require("../controllers/auth");
+
+//* URL: /auth
+
+router.post("/login", auth.login);
+router.post("/refresh", auth.refresh);
+router.get("/logout", auth.logout);
+
+/* ------------------------------------------------------- */
+module.exports = router;
