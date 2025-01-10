c97cc5d2c8fd3b67b0e8ae28c1f285114b852c29
Mahir <mkojak75@gmail.com>
2025-01-10 17:59:05 +0100

    pagination

--- BlogApp_Template/src/controllers/view/blogPostController.js
+++ BlogApp_Template/src/controllers/view/blogPostController.js
@@ -12,13 +12,15 @@ const BlogCategory = require("../../models/blogCategoryModel")
 
 module.exports = {
   list: async (req, res) => {
-    const posts = await res.getModelList(BlogPost,{}, "blogCategoryId");
+    const posts = await res.getModelList(BlogPost,{isPublished:true}, "blogCategoryId");
 
     const categories = await BlogCategory.find({})
 
     const recentPosts = await BlogPost.find().sort({createdAt:'desc'}).limit(3)
 
-    res.render("index", {categories, posts, recentPosts})
+    const details = await res.getModelListDetails(BlogPost, {isPublished: true})
+
+    res.render("index", {categories, posts, recentPosts, details})
     
     // res.status(200).send({
     //   error: false,
--- BlogApp_Template/src/helpers/generatePagination.js
+++ BlogApp_Template/src/helpers/generatePagination.js
@@ -22,13 +22,13 @@ module.exports = function generatePagination(
   // Add appropriate pages at the beginning
   if (current > 2) {
     if (!pagesList.includes(current - 2)) {
-      pagesList = [current - 2, ...pagesList];
+      pagesList = [current - 2, ...pagesList]; //2 3 4 5
     }
   }
 
   // Add appropriate pages at the end
   if (current < totalPages - 2) {
-    if (!pagesList.includes(current + 2)) {
+    if (!pagesList.includes(current + 2)) { // 2 3 4 5 6
       pagesList.push(current + 2);
     }
   }
